b9b10d9f35606cdd00357e2298fe16f795beba80
Danil Kovtonyuk <danx.kov@gmail.com>
2019-03-14 00:53:04 +1000

    Delegate update
    -Update blockchainlabs logo
    -Fix styles
    -Copy Cosmos Val to clipboard on click Delegate
    -Show and hide tooltip after copy Cosmos Val
    -Add environment variables
    -Add footer disclaimer

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,21 @@
 <template>
     <header id="header">
+        <div class="delegate" @click="copyToClipboard">
+            <p>
+                <i class="icon icon-shield"></i>
+                <span>Delegate your Cosmos ATOMS to us to keep network secure and decentralized!</span>
+            </p>
+            <div class="btn btn-copy" id="copy">
+                <span>{{cosmosVal}}</span>
+                <i class="icon icon-copy"></i>
+            </div>
+        </div>
+        <b-tooltip :show.sync="showTooltip" triggers="" target="copy" placement="bottom">
+            Copied
+        </b-tooltip>
          <b-navbar toggleable="md">
             <div class="container">
-                <b-navbar-brand href="#">
+                <b-navbar-brand to="/">
                     <img src="../assets/img/logo.svg" width="41" height="38" class="d-inline-block align-top" alt="">
                     ZTAKE
                 </b-navbar-brand>
@@ -30,6 +43,7 @@ import BNavbarBrand from 'bootstrap-vue/es/components/navbar/navbar-brand'
 import BNavbarNav from 'bootstrap-vue/es/components/navbar/navbar-nav'
 import BNavItem from 'bootstrap-vue/es/components/nav/nav-item'
 import BNavbarToggle from 'bootstrap-vue/es/components/navbar/navbar-toggle'
+import BTooltip from 'bootstrap-vue/es/components/tooltip/tooltip'
 
 export default {
     name: "Header",
@@ -40,6 +54,24 @@ export default {
         'b-navbar-nav': BNavbarNav,
         'b-nav-item': BNavItem,
         'b-navbar-toggle': BNavbarToggle,
+        'b-tooltip': BTooltip
+    },
+    data() {
+        return {
+            showTooltip: false,
+            cosmosVal: process.env.COSMOS_VAL
+        }
+    },
+    methods: {
+        copyToClipboard() {
+            let _this = this;
+            _this.$copyText(_this.cosmosVal).then(function () {
+                _this.showTooltip = true;
+                setInterval(function () {
+                    _this.showTooltip = false;
+                }, 2000);
+            })
+        }
     }
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="ztake">
-    <div id="delegate">
-        <a href="#"><i class="icon icon-shield"></i>Delegate your Cosmos ATOMS to us to keep network secure and decentralized!</a>
-        <a>cosmosvaloper102ruvpv2srmunfffxavttxnhezln6fnc54at8c</a>
-    </div>
     <Header/>
     <nuxt/>
     <Footer/>
